05a1fd836290a78880af97a3d55ff50ba8b2f142
Riccorbypro <riccorbypro@gmail.com>
2025-03-11 08:40:43 +0200

    Added test apps for debugging WDL integration

---
+++ DLTestApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using RGB.NET.Core;
+using RGB.NET.Devices.DynamicLighting;
+using RGB.NET.Presets.Decorators;
+using RGB.NET.Presets.Textures;
+using RGB.NET.Presets.Textures.Gradients;
+
+RGBSurface surface = new RGBSurface();
+surface.Load(DynamicLightingDeviceProvider.Instance);
+surface.AlignDevices();
+
+surface.RegisterUpdateTrigger(new TimerUpdateTrigger());
+
+ILedGroup allLeds = new ListLedGroup(surface, surface.Leds);
+RainbowGradient rainbow = new RainbowGradient();
+rainbow.AddDecorator(new MoveGradientDecorator(surface));
+ITexture texture = new ConicalGradientTexture(new Size(10, 10), rainbow);
+allLeds.Brush = new TextureBrush(texture);
+
+Console.ReadKey();
